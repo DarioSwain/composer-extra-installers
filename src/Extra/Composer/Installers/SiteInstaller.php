dbb51e359bfd08b9b5024516301e900fd42cce36
Ilya Pokamestov <Ilya_Pakamiastau@epam.com>
2015-07-08 19:50:06 +0300

    symlink

--- src/Extra/Composer/Installers/SiteInstaller.php
+++ src/Extra/Composer/Installers/SiteInstaller.php
@@ -5,10 +5,6 @@ class DrupalInstaller extends BaseInstaller
 {
     protected $locations = array(
         'core'      => 'core/',
-        'module'    => 'modules/{$name}/',
-        'theme'     => 'themes/{$name}/',
-        'library'   => 'libraries/{$name}/',
-        'profile'   => 'profiles/{$name}/',
-        'drush'     => 'drush/{$name}/',
+        'platform'    => 'profiles/{$name}/'
     );
 }
